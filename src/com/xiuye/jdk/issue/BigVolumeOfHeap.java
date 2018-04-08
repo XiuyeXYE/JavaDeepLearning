d1e2eb0c359288c3e67369970c7dbf38e0f65b03
xiuye <xiuye_engineer@outlook.com>
2018-04-09 00:06:15 +0800

    new

--- src/com/xiuye/jdk/issue/BigVolumeOfHeap.java
+++ src/com/xiuye/jdk/issue/BigVolumeOfHeap.java
@@ -3,7 +3,7 @@ package com.xiuye.jdk.issue;
 public class BigVolumeOfHeap {
 
 	public static void main(String[] args) {
-		int []a = new int[10000];
+		int []a = new int[10];
 		for(int i=0;i<a.length;i++){
 			a[i] = 100;
 		}
